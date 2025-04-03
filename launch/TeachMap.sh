0344cbe13d10babed426cd71a0de3ac05f3c2ef7
Desiree Fisker <58236510+desifisker@users.noreply.github.com>
2025-04-02 22:24:27 -0400

    Update TeachMap.sh

--- launch/TeachMap.sh
+++ launch/TeachMap.sh
@@ -2,8 +2,7 @@
 # TeachMap.sh - Setup and generate the teach map.
 #
 # Usage: ./TeachMap.sh <point_cloud_ply> <path_csv> <project_name>
-# Example:
-#   ./TeachMap.sh "/home/desiree/ASRL/vtr3/virtual_teach_vtr_wrapper/data/test_press/pointclouds/point_cloud.ply" "/home/desiree/ASRL/vtr3/virtual_teach_vtr_wrapper/data/test_press/paths/relative_transforms.csv" "test_press"
+
 
 if [ "$#" -ne 3 ]; then
   echo "Usage: $0 <point_cloud_ply> <path_csv> <project_name>"
@@ -26,19 +25,21 @@ cd ${VTRROOT}/src/main;
 mkdir -p "${VTRROOT}/data/<PROJECT_NAME>/"; 
 cp "<PC_PATH>" "${VTRROOT}/data/<PROJECT_NAME>/point_cloud.ply"; 
 cp "<CSV_PATH>" "${VTRROOT}/data/<PROJECT_NAME>/nerf_gazebo_relative_transforms.csv"; 
+pip install open3d
+cd ${VTRROOT}/virtual_teach_vtr_wrapper
+colcon build --packages-select vtr_virtualteach
 python3 - <<PYTHON_EOF
 import open3d as o3d
 import os
-vtrroot = os.environ.get("VTRROOT")
-ply_path = os.path.join(vtrroot, "data", "<PROJECT_NAME>", "point_cloud.ply")
+ply_path = os.path.join("${VTRROOT}", "data", "<PROJECT_NAME>", "point_cloud.ply")
 pcd = o3d.io.read_point_cloud(ply_path)
-pcd_path = os.path.join(vtrroot, "data", "<PROJECT_NAME>", "point_cloud.pcd")
+pcd_path = os.path.join("${VTRROOT}", "data", "<PROJECT_NAME>", "point_cloud.pcd")
 o3d.io.write_point_cloud(pcd_path, pcd)
 print("Converted point cloud from .ply to .pcd.")
 PYTHON_EOF
-sed -i 's|/home/desiree/ASRL/vtr3/data/test_press/point_cloud.pcd|/home/desiree/ASRL/vtr3/data/<PROJECT_NAME>/point_cloud.pcd|g' ${VTRROOT}/virtual_teach_vtr_wrapper/src/vtr_virtualteach/src/generate_global_map.cpp; 
-sed -i 's|/home/desiree/ASRL/vtr3/data/test_press/nerf_gazebo_relative_transforms.csv|/home/desiree/ASRL/vtr3/data/<PROJECT_NAME>/nerf_gazebo_relative_transforms.csv|g' ${VTRROOT}/virtual_teach_vtr_wrapper/src/vtr_virtualteach/src/generate_global_map.cpp; 
-sed -i 's|/home/desiree/ASRL/vtr3/data/test_press/graph|/home/desiree/ASRL/vtr3/data/<PROJECT_NAME>/graph|g' ${VTRROOT}/virtual_teach_vtr_wrapper/src/vtr_virtualteach/src/generate_global_map.cpp; 
+sed -i 's|${VTRROOT}/data/test_press/point_cloud.pcd|${VTRROOT}/data/<PROJECT_NAME>/point_cloud.pcd|g' ${VTRROOT}/virtual_teach_vtr_wrapper/src/vtr_virtualteach/src/generate_global_map.cpp; 
+sed -i 's|${VTRROOT}/data/test_press/nerf_gazebo_relative_transforms.csv|${VTRROOT}/data/<PROJECT_NAME>/nerf_gazebo_relative_transforms.csv|g' ${VTRROOT}/virtual_teach_vtr_wrapper/src/vtr_virtualteach/src/generate_global_map.cpp; 
+sed -i 's|${VTRROOT}/data/test_press/graph|${VTRROOT}/data/<PROJECT_NAME>/graph|g' ${VTRROOT}/virtual_teach_vtr_wrapper/src/vtr_virtualteach/src/generate_global_map.cpp; 
 
 source ~/ASRL/vtr3/virtual_teach_vtr_wrapper/install/setup.bash 
 ros2 run vtr_virtualteach generate_global_map
