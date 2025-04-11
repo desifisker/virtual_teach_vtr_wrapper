3eb261dbe2b3b4be5bc3406e4e70a054a0344dc9
Desiree Fisker <58236510+desifisker@users.noreply.github.com>
2025-04-11 15:52:04 -0400

    Update TeachMap.sh

--- launch/TeachMap.sh
+++ launch/TeachMap.sh
@@ -40,7 +40,7 @@ PYTHON_EOF
 
 source ~/ASRL/vtr3/virtual_teach_vtr_wrapper/install/setup.bash 
 colcon build --packages-select vtr_virtualteach
-ros2 run vtr_virtualteach generate_global_map "${VTRROOT}/data/<PROJECT_NAME>/point_cloud.pcd" "${VTRROOT}/data/<PROJECT_NAME>/nerf_gazebo_transformations.csv" "${VTRROOT}/data/<PROJECT_NAME>/graph"
+ros2 run vtr_virtualteach generate_global_map "${VTRROOT}/data/<PROJECT_NAME>/point_cloud.pcd" "${VTRROOT}/data/<PROJECT_NAME>/nerf_gazebo_relative_transformations.csv" "${VTRROOT}/data/<PROJECT_NAME>/graph"
 EOF
 )
 
